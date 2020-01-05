0e23de3bc26fa65143be526d9c4f1eac2a683bca
jmalue <jrod2416@gmail.com>
2020-01-05 14:06:01 -0500

    Swap2Chars

---
+++ src/SwapCharsTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharsTest {
+
+	@Test
+	void swapStringwith2Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));
+	}
+	@Test
+	void swapStringwith6Chars() {
+		assertEquals("AB",SwapChars.swap2("BA"));
+	}
+}
